62e6ab25790f49f50977957a132550c5d5afae28
Andreas Roth <aroth@arsoft-online.com>
2021-05-13 13:12:18 +0200

    note about skip_commits_older_than

--- test.sh
+++ test.sh
@@ -9,10 +9,14 @@ repo_name=`git config hooks.emailprefix`
 if [ -z "$repo_name" ]; then
     git config hooks.emailprefix "$(basename $REPO_DIR)"
 fi
+# if "skip_commits_older_than" is set in "git-commit-notifier.yml" than very often
+# the commits for testing are rejected because they are too old.
+# For testing is advised to remove the "skip_commits_older_than" setting from the
+# git-commit-notifier.yml.
 touch git-commit-notifier.log
 chmod 666 git-commit-notifier.log
 #chown 1000.1000 git-commit-notifier.log
 echo "HEAD^1 HEAD refs/heads/master" | \
-    docker run --rm -i --user "$REPO_UID:$REPO_GID" -v "$REPO_DIR:/git" \
+    docker run --rm -i -w /git --user "$REPO_UID:$REPO_GID" -v "$REPO_DIR:/git" \
     rothan/docker-git-commit-notifier:latest \
     "/git/git-commit-notifier.yml"
